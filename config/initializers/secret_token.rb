3c54c1e310d605d75a9127dcaa5e10684d65dad4
Shiwani <shivanichawla10@yahoo.com>
2014-03-25 18:55:36 +0530

    Intialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecndApp::Application.config.secret_key_base = '4388034535c6967923d36ace99da3e177118cc36a1094fa7480cc6a868f3b1248812a270524f187390620e7407a7f0bb4cba7bb7b7371b71da3fdcf01f9be8e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecndApp::Application.config.session_store :cookie_store, key: '_secnd_app_session'
